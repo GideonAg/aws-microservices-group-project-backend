ccfac827a73e7b926783f9d9f311589896b53906
Gideon Agbosu <99260218+GideonAg@users.noreply.github.com>
2025-04-15 16:30:58 +0000

    Add dynamodb Task and User entities (#3)
    * Change sam template java runtime version
    
    * Add Task and User entities
    
    * Add various classes for lambda functions
    
    * Add AWS region to sam template env
    
    * Modify sns step function execution policy
    
    * Modify sns step function execution policy
    
    * Fix artifact not found in pipeline

--- src/main/java/com/taskmanagementsystem/entities/testfile.java
+++
@@ -1,4 +0,0 @@
-package com.taskmanagementsystem.entities;
-
-public class testfile {
-}
---
+++ src/main/java/com/taskmanagementsystem/notifications/ProcessExpiredTaskHandler.java
@@ -0,0 +1,13 @@
+package com.taskmanagementsystem.notifications;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class ProcessExpiredTaskHandler  implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent requestEvent, Context context) {
+        return null;
+    }
+}
