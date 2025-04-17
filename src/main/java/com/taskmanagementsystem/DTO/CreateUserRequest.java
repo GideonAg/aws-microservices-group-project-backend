bd0a6796eb71c3708cf81c670c97d222f81a1638
Kinglsey <kinglsey@localhost.localdomain>
2025-04-17 15:07:13 +0100

    feat:creat user request dto implementation

--- src/main/java/com/taskmanagementsystem/DTO/CreateUserRequest.java
+++ src/main/java/com/taskmanagementsystem/DTO/CreateUserRequest.java
@@ -1,5 +1,40 @@
-package com.taskmanagementsystem.DTO;
-
+package  com.taskmanagementsystem.DTO;
+// Request DTO
 public class CreateUserRequest {
+    private String email;
+    private String role;
+    private String firstName;
+    private String lastName;
     
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getRole() {
+        return role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
